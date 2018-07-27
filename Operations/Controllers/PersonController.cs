d94befbe437eb282d62cbde206c421c7686c7cef
Ashwin Kumar H <ashwinkpes@hotmail.com>
2018-07-27 23:59:37 +0530

    Added personcontroller

---
+++ Operations/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Operations.Models;
+
+namespace Operations.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Person")]
+    public class PersonController : Controller
+    {
+        private readonly PersonDbContext personDbContext;
+
+        public PersonController(PersonDbContext personDbContext)
+        {
+            this.personDbContext = personDbContext;
+        }
+
+        public async Task<IEnumerable<Person>> GetPeople()
+        {
+            var people = await this.personDbContext.People.ToListAsync().ConfigureAwait(false);
+            return people;
+        }
+    }
+}
